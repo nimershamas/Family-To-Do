b04c336fafbcb0f38d11ad9210e84249dd6337d2
Nimer <nemrsh1@gmail.com>
2020-08-17 13:40:37 +0300

    added

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+
+const app = express();
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || 4000;
+app.listen(port,()=>(console.log("server listen on port 4000")));
+
