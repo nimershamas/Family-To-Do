fe1c448ac766f354deacba7a4876efba5e74cca1
Nimer <nemrsh1@gmail.com>
2020-08-17 15:05:06 +0300

    first step

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
-const express = require ("express");
+const express = require("express");
+const { Schema, Mongoose } = require("mongoose");
 
 const app = express();
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
 app.use(express.static('public'));
 
+
+const url = "mongodb+srv://nimer:N1N1N1N1@cluster0.tejcy.mongodb.net/toDo";
+
+
+const mongoose = require('mongoose');
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const User = Mongoose.model("User", {
+    userEmail: String,
+    firstName: String,
+    lastName: String,
+    imgUrl: String,
+    password: String
+})
+
+const Task = Mongoose.mode("Task", {
+    user: {
+        userEmail: String,
+        firstName: String,
+        lastName: String,
+        imgUrl: String,
+        password: String
+    },
+    taskTitle: String,
+    taskContent: String,
+    done: Boolean
+})
+
+
+
+
+
+
+
+
+
 const port = process.env.PORT || 4000;
-app.listen(port,()=>(console.log("server listen on port 4000")));
+app.listen(port, () => (console.log("server listen on port 4000")));
 
